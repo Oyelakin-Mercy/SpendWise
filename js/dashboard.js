833ea0c391e76eb03e059b130267b1cc8e32290c
Oyelakin-Mercy <oyelakinmercy21@gmail.com>
2026-03-23 02:42:24 -0700

    redirect signout to index page

--- js/dashboard.js
+++ js/dashboard.js
@@ -50,7 +50,7 @@ const logoutBtn = document.getElementById('logout-btn');
 if (logoutBtn) {
     logoutBtn.addEventListener('click', async () => {
         await supabase.auth.signOut();
-        window.location.href = '/pages/login.html';
+        window.location.href = '/index.html';
     });
 }
 
